66cd35fa358b0bda0e71bb64fc7dfe8007eda844
ssmelo <samuelmelo00@hotmail.com>
2021-04-21 12:57:38 -0300

    Database configuration and settings table

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import "./database"; // o javascript reconhece q queremos o index. ele tambem executa automaticamente o import
+import { routes } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({
-        "status": 500,
-    })
-})
+app.use(express.json());
+
+app.use(routes);
+
 app.listen(3333,() => {
     console.log("Server is running on port 3333");
 });
